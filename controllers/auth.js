30bb2f8aed1508062863ae455bf1b31ea696d7dd
Sixto José Romero Martínez <sixto.jose@gmail.com>
2024-04-25 15:41:55 -0500

    Se aplican cambios para permitir guardar un nuevo usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,20 +10,21 @@ const userDataPath = path.join(__dirname, '../data/users.json');
 const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
 
 const login = async(req, res = response) => {
-    const { correo, password } = req.body;
+    
+    const { email, password } = req.body;
 
     try {        
         // Verificar si el email existe
-        const usuario = userData.find(u => u.email === correo);
+        const usuario = userData.find(u => u.email === email);
 
         if (!usuario) {
             return res.status(404).json({ msg: 'Usuario / Password no son correctos - correo' });
-        }
+        }        
 
-        //Verificar la contraseña
+        //Verificar la contraseña        
         const validPassword = bcryptjs.compareSync(password, usuario.password);
         if (!validPassword){
-            return res.status(404).json({ msg: 'Usuario / Password no son correctos - password' });
+            return res.status(404).json({ msg: 'Usuario / Password no son correctos - password',  password });
         }
 
         //Generar el JWT
@@ -37,11 +38,46 @@ const login = async(req, res = response) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            msg: 'Hable con el administrador'
+            msg: 'Ha ocurrido un error inesperado, hable con el administrador'
         });
     }    
 }
 
+const authPost = (req, res) => {            
+
+    const id = userData.length + 1;
+
+    const {email, password} = req.body;
+
+    const usuario = userData.find(u => u.email === email);
+
+    if (usuario) {
+        return res.status(404).json({ msg: 'El correo ya existe en la base de datos.' });
+    }    
+
+    const users = userData;
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    const passCrypt = bcryptjs.hashSync( password, salt );    
+
+    const newUser = {
+        id,
+        email,
+        "password": passCrypt
+    };
+
+    // Añadir el nuevo ítem
+    users.push(newUser);
+
+    // Escribir el array actualizado de vuelta al archivo JSON
+    fs.writeFileSync(userDataPath, JSON.stringify(users, null, 2));
+
+    res.status(201).send({ msg: 'Usuario registrado con éxito!', newUser });
+
+}
+
 module.exports = {
-    login
+    login,
+    authPost
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,23 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, authPost } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.post('/login', [
-    check('correo', 'El correo es obligatorio').isEmail(),
+    check('email', 'El correo es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatorio').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/create',[
+    check('email', 'El correo es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatorio').not().isEmpty(),
+    validarCampos,
+    validarJWT
+], authPost);
 
 module.exports = router;
